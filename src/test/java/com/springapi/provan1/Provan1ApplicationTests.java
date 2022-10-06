d107a1ca59724171cc7f993d91c2845c08570877
flavia@flavia-Inspiron-3542 <flavinha.goncalves@gmail.com>
2022-10-05 23:49:37 -0300

    deploy teste

---
+++ src/test/java/com/springapi/provan1/Provan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapi.provan1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
