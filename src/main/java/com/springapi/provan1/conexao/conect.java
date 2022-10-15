055332a37b68caefd1661b52d4c9eefe03c3a833
flavia@flavia-Inspiron-3542 <flavinha.goncalves@gmail.com>
2022-10-15 18:17:39 -0300

    Alteradas configurações de banco

--- src/main/java/com/springapi/provan1/conexao/conect.java
+++ src/main/java/com/springapi/provan1/conexao/conect.java
@@ -9,7 +9,7 @@ public class conect {
         throws ClassNotFoundException, SQLException{
             Connection conexao = null;
             Class.forName("com.mysql.jdbc.Driver");
-            conexao = DriverManager.getConnection("us-cdbr-east-06.cleardb.net/heroku_b352466dfb92954","b5364f73e03359","7df6cfa2");
+            conexao = DriverManager.getConnection("us-cdbr-east-06.cleardb.net/heroku_5344e9f41e8d916","b42e311f924484","f1534093");
             return conexao;
         }
         
